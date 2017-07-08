06ea6be3f33375cf9049f876e85094f07f6fb650
Vincent-Bryan <2315223212@qq.com>
2017-07-09 00:39:17 +0800

    add 2 txt

--- huffman/test001.py
+++ huffman/test001.py
@@ -1,26 +1,44 @@
 import sys
+import six
+g_code = {101:'0', 122:'100', 100:'101', 99:'11'}
+
 if __name__ == '__main__':
-	if len(sys.argv) != 2:
+	if len(sys.argv) != 3:
 		print("Please input a filename")
 		exit(0)
 	else:
-		INPUTFILE = sys.argv[1]
+		input_file = sys.argv[1]
+		output_file = sys.argv[2]
 
-	f = open(INPUTFILE, 'rb')
+	f = open(input_file, 'rb')
+	output_ptr = open(output_file, 'wb')
 	file_data = f.read()
 	file_size = f.tell()
 
 	char_freq = {}
+	content = ''
+	cnt = 0
 	for x in range(file_size):
 		tmp = file_data[x]
-		# print(tmp.__class__.__name__)
-		# print(str(tmp))
-		tt = str(tmp).encode()
-		print(len(tmp), len(tt), tt[0], tt[1])
-		if tmp in char_freq.keys():
-			char_freq[tmp] = char_freq[tmp] + 1
-		else:
-			char_freq[tmp] = 1
+		# print(tmp)
+		content = content + g_code[tmp]
+		out = 0
+		while len(content) > 8:
+			cnt = cnt + 1
+			for i in range(8):
+				out = out << 1
+				if content[i] == '1':
+					out = out | 1
+			content = content[8:]
+			# print(six.int2byte(out))
+			output_ptr.write(six.int2byte(out))
+			out = 0
+		# if tmp in char_freq.keys():
+		# 	char_freq[tmp] = char_freq[tmp] + 1
+		# else:
+		# 	char_freq[tmp] = 1
+	# print(cnt)
 
-	for tmp in char_freq.keys():
-		print(chr(tmp),char_freq[tmp])
+
+	# for tmp in char_freq.keys():
+	# 	print(chr(tmp),char_freq[tmp])
